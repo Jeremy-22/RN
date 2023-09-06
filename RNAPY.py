7557702fabde807bf32561da9f6b9aec01c0772e
jeremy-22 <jeryrangmart@gmail.com>
2023-09-06 14:02:21 -0600

    comment 8

--- RNAPY.py
+++ RNAPY.py
@@ -19,13 +19,13 @@ class Network(object):       # clase del objeto con argumneto su herencia, es de
         self.beta = 0.9
         #Los pesos (w) son una matriz que relaciona las flechas entre una capa y otra
         #Esto es para entrenar la red neuronal y poderla usar lo siguiente
-def feedforward(self, a): #Se define feedforward, para evaluar la red neuronal
+    def feedforward(self, a): #Se define feedforward, para evaluar la red neuronal
                            #(self, a(activaciones de la primera capa))
         """Return the output of the network if "a" is input."""
         for b, w in zip(self.biases, self.weights):   # zip pega los vectores elemento a elemento
             a = sigmoid(np.dot(w, a)+b)
         return a
-def SGD(self, training_data, epochs, mini_batch_size, eta,
+    def SG(self, training_data, epochs, mini_batch_size, eta,
             test_data=None):
 #Aquí se definio el Stochastic Gradient Descent, con trainig data como la lista de Tuplas "(x,y)"
 
@@ -38,19 +38,20 @@ def SGD(self, training_data, epochs, mini_batch_size, eta,
         #network will be evaluated against the test data after each
         #epoch, and partial progress printed out.  This is useful for
         #tracking progress, but slows things down substantially."""
-        if test_data: n_test = len(test_data) #trasformar los datos de entrenamiento en una lista, o tupla
+        training_data = list(training_data)
+        #if test_data: n_test = len(test_data) #trasformar los datos de entrenamiento en una lista, o tupla
         n = len(training_data)  #El número de datos tenemos para entrenar
 
         if test_data:
             test_data = list(test_data)
             n_test = len(test_data) #numero de datos de prueba
 
-        for j in xrange(epochs): #Es para iterar sobre el numero de epocas que se deasea entrenar
+        for j in range(epochs): #Es para iterar sobre el numero de epocas que se deasea entrenar
             random.shuffle(training_data) # Es como barajar los elementos de la lista 
             mini_batches = [
                 training_data[k:k+mini_batch_size]# el tamaño de los mini_batches que se utilixzara 
                                             # para hacer el muestreo
-                for k in xrange(0, n, mini_batch_size)] # donde eta es la taza de aprendizaje
+                for k in range(0, n, mini_batch_size)] # donde eta es la taza de aprendizaje
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
@@ -60,7 +61,7 @@ def SGD(self, training_data, epochs, mini_batch_size, eta,
             #Si se proporciona el argumento opcional test_data, 
             #entonces el programa evaluará la red después de cada época de entrenamiento e 
             #imprimirá el progreso parcial.
-def update_mini_batch(self, mini_batch, eta): #para cada mini_batch se aplica un único paso de descenso 
+    def update_mini_batch(self, mini_batch, eta): #para cada mini_batch se aplica un único paso de descenso 
                                              #del gradiente.
         """Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
@@ -81,7 +82,7 @@ def update_mini_batch(self, mini_batch, eta): #para cada mini_batch se aplica un
                        for b, nb in zip(self.biases, nabla_b)]
             #Se comienza mezclando aleatoriamente los datos de entrenamiento y luego los divide en 
             #minilotes del tamaño apropiado.
-def backprop(self, x, y): #esto nos devuelveuna tupla que representa el
+    def backprop(self, x, y): #esto nos devuelveuna tupla que representa el
          #gradiente para la función de costo C_x
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]#son listas de capa por capa o elemento a
@@ -97,7 +98,7 @@ def backprop(self, x, y): #esto nos devuelveuna tupla que representa el
             activations.append(activation)
         # backward pass
         delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
+              sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
         
@@ -111,21 +112,21 @@ def backprop(self, x, y): #esto nos devuelveuna tupla que representa el
                                       #sesgos y pesos
         return (nabla_b, nabla_w)
     
-def evaluate(self, test_data):#numero de datos que acerto la red
+    def evaluate(self, test_data):#numero de datos que acerto la red
         
         test_results = [(np.argmax(self.feedforward(x)), y)
                         for (x, y) in test_data] #Los datos de prueba son una lista de tuplas
         return sum(int(x == y) for (x, y) in test_results)# devuelve l número de entradas de prueba 
                                             #para las cuales la red neuronal genera el resultado correcto.
 
-def cost_derivative(self, output_activations, y): #derivada de la funcion de costo
+    def cost_derivative(self, output_activations, y): #derivada de la funcion de costo
         
         return (output_activations-y) #esto nos devuelve el vector de derivadas parciales de la funcion
         #de costo 
 
 #### Miscellaneous functions
-def sigmoid(z):
-    return 1.0/(1.0+np.exp(-z)) #nos regresa la funcion sigmoidal de la capa  
+    def sigmoid(z):
+        return 1.0/(1.0+np.exp(-z)) #nos regresa la funcion sigmoidal de la capa  
 
-def sigmoid_prime(z):
-    return sigmoid(z)*(1-sigmoid(z)) #nos regresa la derivada de la funcion sigmoidal de la capa  
+    def sigmoid_prime(z):
+        return sigmoid(z)*(1-sigmoid(z)) #nos regresa la derivada de la funcion sigmoidal de la capa  
