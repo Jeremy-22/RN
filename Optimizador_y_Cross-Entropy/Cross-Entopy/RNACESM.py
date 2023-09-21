9e318e990803966ef39bb232858779bdb473ee5c
jeremy-22 <jeryrangmart@gmail.com>
2023-09-21 00:22:03 -0600

    intento de implementar cross entropy

--- Optimizador_y_Cross-Entropy/Cross-Entopy/RNACESM.py
+++ Optimizador_y_Cross-Entropy/Cross-Entopy/RNACESM.py
@@ -1,27 +1,37 @@
 import random
 import numpy as np
 
-class CrossEntropy(object): #definimos a cross-entropy.
-    def f(a,y): #funcion de costo.
+def CrossEntropy(a,y): #funcion de costo.
         training_data = list(training_data)
         n = len(training_data)
         r = 1/n * np.sum(y*np.log(a)+(1-y)*np.log(1-a))#donde r es binary cross entropy.
+
         return r
+def softmax(z):
+        return np.exp(z)/(np.dot(z)*np.exp(z))
+def CEloss(a,y):
+       training_data = list(training_data)
+       n = len(training_data)
+       cels = 1/n*np.sum(y*np.log(a))
+       return cels
 class Network(object):       
-    def __init__(self, sizes, cost=CrossEntropy):#añadimos a network el crossentropy.
+    def __init__(self, sizes, cost=CrossEntropy, sm=softmax, cels=CEloss):#añadimos a network el crossentropy.
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.cost = cost #asigna el valor de la variable cost al atributo cost.
-
+        self.sm = sm
+        self.cels = cels
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) 
                         for x, y in zip(sizes[:-1], sizes[1:])]
         self.epsilon =0.00001
 
+    
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
+             #a = Network.sigmoid(np.dot(w, a)+b)
              a = Network.sigmoid(np.dot(w, a)+b)
-        return a
+        return a 
     ## Implementación de Backprop con inercia
     def SGD_momentum(self, training_data, epochs, mini_batch_size, eta, momentum,
             test_data=None):
@@ -57,11 +67,13 @@ class Network(object):
                       for vw, nw in zip(v_w, nabla_w)]
         self.biases = [b + (eta / len(mini_batch))*vb for b, vb in zip(self.biases, v_b)]
         self.weights = [w + (eta / len(mini_batch))*vw for w, vw in zip(self.weights, v_w)]
+       
+
 ##
     def backprop(self, x, y):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        activation = x
+        activation = self.cels
         activations = [x] 
         zs = []
         for b, w in zip(self.biases, self.weights):
@@ -89,6 +101,13 @@ class Network(object):
 
     def cost_derivative(self, output_activations, y):
         return (output_activations-y) 
+    def softmax(z):
+        return np.exp(z)/(np.dot(z)*np.exp(z))
+    def grad_crossentropy(y, z):
+        training_data = list(training_data)
+        sp = Network.sigmoid_prime(z)
+        n = len(training_data)
+        return -1/n * np.sum(y/Network.sigmoid(z) - (1-y)/(1-Network.sigmoid(z))*sp)
     def sigmoid(z):
         return 1.0/(1.0+np.exp(-z))
     def sigmoid_prime(z):
